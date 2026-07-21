486deb2d2bd9fae9ddacf54423cff99a8e1f89ab
Parth Patel <pateltilesceramic@gmail.com>
2026-07-21 11:25:00 +0530

    Set center logo imageSize to 1.0 and disable background dot hiding to minimize white gaps

--- src/app/qr-studio/page.tsx
+++ src/app/qr-studio/page.tsx
@@ -172,8 +172,8 @@ export default function QRStudioPage() {
       imageOptions: {
         crossOrigin: "anonymous",
         margin: 0,
-        imageSize: 0.78,
-        hideBackgroundDots: true
+        imageSize: 1.0,
+        hideBackgroundDots: false
       },
       cornersSquareOptions: {
         type: "extra-rounded",
